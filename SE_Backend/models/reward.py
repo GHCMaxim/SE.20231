ce9af79ea516cd7ecbb077062e83f0705ed71c6a
GHCMaxim <maxim080503@gmail.com>
2023-12-28 23:34:21 +0700

    fix: correct recipient type

--- SE_Backend/models/reward.py
+++ SE_Backend/models/reward.py
@@ -1,6 +1,9 @@
 import datetime
+import uuid
+
 from sqlalchemy import Boolean, Date, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+
 from ..database import Base
 
 
@@ -10,7 +13,7 @@ class Reward(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     reward_type_id: Mapped[int] = mapped_column(ForeignKey("reward_types.id"))
     date: Mapped[datetime.date] = mapped_column(Date, nullable=False)
-    recipient: Mapped[str] = mapped_column(ForeignKey("people.id"))
+    recipient: Mapped[uuid.UUID] = mapped_column(ForeignKey("people.id"))
     spending_id: Mapped[int] = mapped_column(ForeignKey("spendings.id"))
 
 
