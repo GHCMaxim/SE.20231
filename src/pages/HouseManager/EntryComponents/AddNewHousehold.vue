4d3995f54919b098f000655482c2301ca8e19413
GHCMaxim <101856461+GHCMaxim@users.noreply.github.com>
2024-01-12 00:33:28 +0700

    feat(UI): fixed AddNewHousehold

--- src/pages/HouseManager/EntryComponents/AddNewHousehold.vue
+++ src/pages/HouseManager/EntryComponents/AddNewHousehold.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import OneColFormWrapper from "../../../components/OneColFormWrapper.vue";
 import InputBox from "../../../components/InputBox.vue";
 import { ref } from "vue";
 import { API } from "../../../store";
@@ -9,6 +8,8 @@ const name = ref("");
 const creation_date = ref("");
 const location = ref("");
 const owner = ref("");
+const house_type = ref("");
+const house_size = ref("");
 
 const message = ref("");
 
@@ -46,44 +47,36 @@ async function handleAddNewHousehold() {
 
 <template>
 	<form @submit.prevent="handleAddNewHousehold()">
-	<OneColFormWrapper class="w-full">
-		<InputBox
-			title="Số hộ khẩu"
-			placeholder="Nhập số hộ khẩu"
-			@update="household_ssn = $event.value"
-			type="number"
-			warn = "Số sổ hộ khẩu chỉ có số"
-		/>
-		<InputBox
-			title="Số tên căn hộ"
-			placeholder="N02-1810"
-			@update="name = $event.value"
-		/>
-		<InputBox
-			title="Số địa chỉ hộ khẩu"
-			placeholder="Nhập địa chỉ"
-			@update="location = $event.value"
-		/>
-		<InputBox
-			title="Số căn cước chủ hộ khẩu"
-			placeholder="Nhập số căn cước"
-			@update="owner = $event.value"
-			type="number"
-			warn = "Số căn cước chỉ có số"
-		/>
-		<InputBox
-			title="Số ngày thành lập sổ hộ khẩu"
-			placeholder="DD/MM/YYYY"
-			@update="creation_date = $event.value"
-			type="date"
-		/>
+		<div class="mx-auto mb-10 mt-8 grid w-4/5 max-w-2xl grid-cols-2 gap-x-10 gap-y-7">
+			<InputBox title="Số hộ khẩu" placeholder="Nhập số hộ khẩu" @update="household_ssn = $event.value" type="number"
+				warn="Số sổ hộ khẩu chỉ có số" />
+			<InputBox title="Tên căn hộ" placeholder="N02-1810" @update="name = $event.value" />
+			<InputBox title="Số căn cước chủ hộ khẩu" placeholder="Nhập số căn cước" @update="owner = $event.value"
+				type="number" warn="Số căn cước chỉ có số" />
+			<InputBox title="Số ngày thành lập sổ hộ khẩu" placeholder="DD/MM/YYYY" @update="creation_date = $event.value"
+				type="date" />
+			<div>
+				<div class="mx-auto mb-2 text-start">Loại căn hộ</div>
+				<select v-model="house_type" class="select select-bordered w-full">
+					<option disabled selected value="">Chọn loại chung cư</option>
+					<option value="1">Chung cư giá rẻ </option>
+					<option value="2">Chung cư thường</option>
+					<option value="3">Chung cư cao cấp</option>
+				</select>
+			</div>
+			<InputBox title="Diện tích căn hộ (m2)" placeholder="Nhập diện tích" @update="house_size = $event.value"
+				type="number" warn="Diện tích chỉ có số" />
 
-		<button 
-			type = "submit"
-			class="btn btn-primary w-full">
-			Tạo mới
-		</button>
+			<div class="mx-auto flex h-full w-full max-w-2xl flex-col items-center justify-center col-span-2">
+				<div class="mb-2 text-start w-full">Địa chỉ</div>
+				<input type="text" placeholder="Nhập địa chỉ" class="input input-bordered w-full mb-4"
+					@update="location = $event.value" />
+				<button type="submit" class="btn btn-primary w-80 self-center">
+					Tạo mới
+				</button>
+			</div>
+		</div>
 		<div v-if="message">{{ message }}</div>
-	</OneColFormWrapper>
+
 	</form>
 </template>
