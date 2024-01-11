960d6c40eddd6a9c7a50a060c5ee760f3bc773c2
GHCMaxim <101856461+GHCMaxim@users.noreply.github.com>
2024-01-11 15:15:07 +0700

    feat(init): Added default payment types

---
+++ SE_Backend/routes/default_init.py
@@ -0,0 +1,81 @@
+import uuid
+from datetime import datetime
+
+from passlib.context import CryptContext
+
+from SE_Backend import models
+from SE_Backend.database import SessionLocal
+
+db = SessionLocal()
+
+person_id = uuid.uuid4()
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+
+payment_type1 = models.PaymentType(
+    name="Phí gửi xe máy (tháng)",
+    rate=70000,
+    active=True,
+    id=1,
+)
+
+payment_type2 = models.PaymentType(
+    name = "Phí gửi ô tô (tháng)",
+    rate = 1200000,
+    active = True,
+    id = 2,
+)
+
+payment_type3 = models.PaymentType(
+    name = "Tiền quản lý CC giá rẻ",
+    rate = 7000,
+    active = True,
+    id = 3,
+)
+
+payment_type4 = models.PaymentType(
+    name = "Tiền quản lý CC thường",
+    rate = 10000,
+    active = True,
+    type = 4,
+)
+
+payment_type5 = models.PaymentType(
+    name = "Tiền quản lý CC cao cấp",
+    rate = 15000,
+    active = True,
+    type = 5,
+)
+
+payment_type6 = models.PaymentType(
+    name = "Tiền điện/số",
+    rate = 2500,
+    active = True,
+    type = 6,
+)
+
+payment_type7 = models.PaymentType(
+    name = "Tiền nước/số",
+    rate = 8000,
+    active = True,
+    type = 7,
+)
+
+payment_type8 = models.PaymentType(
+    name = "Tiền internet",
+    rate = 150000,
+    active = True,
+    type = 8,
+)
+
+db.add(payment_type1)
+db.add(payment_type2)
+db.add(payment_type3)
+db.add(payment_type4)
+db.add(payment_type5)
+db.add(payment_type6)
+db.add(payment_type7)
+db.add(payment_type8)
+db.commit()
+db.close()
